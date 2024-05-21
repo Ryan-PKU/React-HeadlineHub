76ddfe35cadb9ccf48218cac0445f7510526631c
ryan <ryan18pku@gmail.com>
2024-05-21 15:06:26 +0800

    second-try

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,10 +1,60 @@
+import { Layout, Menu, Popconfirm } from 'antd'
+import {
+    HomeOutlined,
+    DiffOutlined,
+    EditOutlined,
+    LogoutOutlined,
+} from '@ant-design/icons'
+import './index.scss'
 
-const Layout = () => {
+const { Header, Sider } = Layout
+
+const items = [
+    {
+        label: 'Home',
+        key: '1',
+        icon: <HomeOutlined />,
+    },
+    {
+        label: 'Article Management',
+        key: '2',
+        icon: <DiffOutlined />,
+    },
+    {
+        label: 'Create Article',
+        key: '3',
+        icon: <EditOutlined />,
+    },
+]
+
+const GeekLayout = () => {
     return (
-        <div>
-            Layout Page
-        </div>
+        <Layout>
+            <Header className="header">
+                <div className="logo" />
+                <div className="user-info">
+                    <span className="user-name">Someone</span>
+                    <span className="user-logout">
+                        <Popconfirm title="Log out?" okText="Log out" cancelText="Cancel">
+                            <LogoutOutlined /> Log out
+                        </Popconfirm>
+                    </span>
+                </div>
+            </Header>
+            <Layout>
+                <Sider width={200} className="site-layout-background">
+                    <Menu
+                        mode="inline"
+                        theme="dark"
+                        defaultSelectedKeys={['1']}
+                        items={items}
+                        style={{ height: '100%', borderRight: 0 }}></Menu>
+                </Sider>
+                <Layout className="layout-content" style={{ padding: 20 }}>
+                    Content
+                </Layout>
+            </Layout>
+        </Layout>
     )
 }
-
-export default Layout
+export default GeekLayout
